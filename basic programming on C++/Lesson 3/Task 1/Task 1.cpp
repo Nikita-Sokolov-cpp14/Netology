722ec63408df44dd0a87fa4050706f6df1e98473
Nikita Sokolov <nekitsok10@yandex.ru>
2023-10-26 17:53:28 +0300

    Finished lesson 3

---
+++ basic programming on C++/Lesson 3/Task 1/Task 1.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+
+class Calculator {
+private:
+    double num1;
+    double num2;
+public:
+    double add() { return num1 + num2; }
+    double multiply() { return num1 * num2; }
+    double subtract_1_2() { return num1 - num2; }
+    double subtract_2_1() { return num2 - num1; }
+    double divide_1_2() { return num1 / num2; }
+    double divide_2_1() { return num2 / num1; }
+    bool set_num1(double num1) {
+        if (num1 != 0) {
+            this->num1 = num1;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    bool set_num2(double num2) {
+        if (num2 != 0) {
+            this->num2 = num2;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    while (true)
+    {
+        Calculator calc_1;
+        double num1, num2;
+        std::cout << "Введите num1: ";
+        std::cin >> num1;
+        while (!calc_1.set_num1(num1)) {
+            std::cout << "Неверный ввод! ";
+            std::cout << "Введите num1: ";
+            std::cin >> num1;
+        }
+
+        std::cout << "Введите num2: ";
+        std::cin >> num2;
+        while (!calc_1.set_num2(num2)) {
+            std::cout << "Неверный ввод! ";
+            std::cout << "Введите num2: ";
+            std::cin >> num2;
+        }
+
+        std::cout << "num1 + num 2 = " << calc_1.add() << std::endl;
+        std::cout << "num1 * num 2 = " << calc_1.multiply() << std::endl;
+        std::cout << "num1 - num 2 = " << calc_1.subtract_1_2() << std::endl;
+        std::cout << "num2 - num 1 = " << calc_1.subtract_2_1() << std::endl;
+        std::cout << "num1 / num 2 = " << calc_1.divide_1_2() << std::endl;
+        std::cout << "num2 / num 1 = " << calc_1.divide_2_1() << std::endl;
+    }
+    return 0;
+}
---
+++ basic programming on C++/Lesson 3/Task 2/Task 2.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <string>
+
+class Counter {
+private:
+    int count;
+public:
+    void set_count(int count) { this->count = count; }
+    Counter() {
+        this->count = 1;
+    }
+    void step(char comand)
+    {
+        switch (comand)
+        {
+            case '+': count++; break;
+            case '-': count--; break;
+            case '=': std::cout << count << std::endl; break;
+            case 'x': std::cout << "До свидаия!"; break;
+            default: std::cout << "Ошибка!\n";
+        }
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    Counter counter;
+    std::string set_init;
+    int initial;
+    char comand;
+
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите Yes или No: ";
+    std::cin >> set_init;
+    if (set_init == "Yes")
+    {
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> initial;
+        counter.set_count(initial);
+    }
+    else
+    {
+        std::cout << "Выбрано значение по умолчанию = 1" << std::endl;
+    }
+
+    std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+    std::cin >> comand;
+    counter.step(comand);
+
+    while (comand != 'x')
+    {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> comand;
+        counter.step(comand);
+    }
+
+    return 0;
+}
