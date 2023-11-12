75623fc2a54d67c08c82fde0aab7131fda169a8c
Nikita Sokolov <nekitsok10@yandex.ru>
2023-11-12 12:39:31 +0300

    complete lesson 8

---
+++ basic programming on C++/lesson 8/Task 1/Task 1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+#define MODE 1
+#if !defined MODE
+#error MODE is not defined
+#endif
+
+#if MODE == 1
+void add() {
+    int a, b;
+    std::cout << "Раотаю в боеоом режиме" << std::endl;
+    std::cout << "Введите 2 числа. Число 1 = ";
+    std::cin >> a;
+    std::cout << "Число 2 = ";
+    std::cin >> b;
+    std::cout << "Сумма = " << a + b << std::endl;
+}
+#endif
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+#if MODE == 0
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+    add();
+#else
+    std::cout << "Неизвестный режим. Завершение работы." << std::endl;
+#endif
+    return 0;
+}
