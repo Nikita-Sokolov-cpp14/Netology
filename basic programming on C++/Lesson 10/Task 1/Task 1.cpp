e1757be33b1231743af545ff02f3b0cdf68bf119
Nikita Sokolov <nekitsok10@yandex.ru>
2023-11-17 17:19:23 +0300

    complete lesson 10

---
+++ basic programming on C++/Lesson 10/Task 1/Task 1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+	bool operator==(Fraction right) {
+	//из приведения дробей к общему значенателю следует
+		int num_1 = numerator_ * right.denominator_;
+		int num_2 = right.numerator_ * denominator_;
+		return num_1 == num_2;
+	}
+	bool operator<(Fraction right) {
+		int num_1 = numerator_ * right.denominator_;
+		int num_2 = right.numerator_ * denominator_;
+		return num_1 < num_2;
+	}
+};
+
+int main()
+{
+	Fraction f1(4, 6);
+	Fraction f2(1, 2);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << (!(f1 == f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f2 < f1) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << (!(f2 < f1) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << (!(f1 < f2) ? " >= " : " not >= ") << "f2" << '\n';
+	return 0;
+}
---
+++ basic programming on C++/Lesson 10/Task 2/Task 2.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+	Fraction operator+(Fraction right) {
+		int numerator = numerator_ * right.denominator_ + denominator_ * right.numerator_;
+		int denominator = denominator_ * right.denominator_;
+		return Fraction(numerator, denominator);
+	}
+	Fraction operator-(Fraction right) {
+		int numerator = numerator_ * right.denominator_ - denominator_ * right.numerator_;
+		int denominator = denominator_ * right.denominator_;
+		return Fraction(numerator, denominator);
+	}
+	Fraction operator*(Fraction right) {
+		return Fraction(numerator_ * right.numerator_, denominator_ * right.denominator_);
+	}
+	Fraction operator/(Fraction right) {
+		int numerator = numerator_ * right.denominator_;
+		int denominator = denominator_ * right.numerator_;
+		return Fraction(numerator, denominator);
+	}
+	Fraction& operator++() {
+		numerator_ += denominator_;
+		return *this;
+	}
+	Fraction& operator--(int) {
+		numerator_ -= denominator_;
+		return *this;
+	}
+	friend std::ostream& operator<<(std::ostream& left, Fraction right);
+};
+std::ostream& operator<<(std::ostream& left, Fraction right) {
+	return (left << right.numerator_ << "/" << right.denominator_);
+}
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int num_1, denom_1;
+	int num_2, denom_2;
+	std::cout << "Введите числитель дроби 1:";
+	std::cin >> num_1;
+	std::cout << "Введите знаменатель дроби 1:";
+	std::cin >> denom_1;
+	std::cout << "Введите числитель дроби 2:";
+	std::cin >> num_2;
+	std::cout << "Введите знаменатель дроби 2:";
+	std::cin >> denom_2;
+	Fraction f_1(num_1, denom_1), f_2(num_2, denom_2);
+	std::cout << f_1 << " + " << f_2 << " = " << f_1 + f_2 << std::endl;
+	std::cout << f_1 << " - " << f_2 << " = " << f_1 - f_2 << std::endl;
+	std::cout << f_1 << " * " << f_2 << " = " << f_1 * f_2 << std::endl;
+	std::cout << f_1 << " / " << f_2 << " = " << f_1 / f_2 << std::endl;
+	std::cout << "++" << f_1 << " * " << f_2 << " = ";
+	std::cout << ++f_1 * f_2 << std::endl;
+	std::cout << "Значение дроби 1 = " << f_1 << std::endl;
+	std::cout << f_1 << "--" << " * " << f_2 << " = ";
+	std::cout << f_1-- * f_2 << std::endl;
+	std::cout << "Значение дроби 1 = " << f_1 << std::endl;
+	return 0;
+}
