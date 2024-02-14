18e6cd67a723d83c8bfe4bbbb37db5b15d4f4506
Nikita Sokolov <nekitsok10@yandex.ru>
2024-02-14 19:06:37 +0300

    complete lesson 6

---
+++ advanced programing/L6 STL_2/Task 1/Task 1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+template<class T>
+std::vector<T> unique_ver_1(std::vector<T>& vec) {
+    std::unordered_map<T, int> buf;
+
+    for (const auto& val : vec) {
+        buf[val]++;
+    }
+
+    std::vector<T> uniq_vec;
+
+    for (const auto& pair : buf) {
+
+        if (pair.second == 1) {
+            uniq_vec.push_back(pair.first);
+        }
+    }
+
+    return uniq_vec;
+}
+
+
+
+int main()
+{
+    std::vector<int> vec_1 = { 1, 2, 3, 2, 2, 6, 3, 2 };
+
+    std::vector<int> vec_3 = unique_ver_1<int>(vec_1);
+
+    for (const auto& value : vec_1) {
+        std::cout << value << "\t";
+    }
+
+    std::cout << "\nafter unique" << std::endl;
+    for (const auto& value : vec_3) {
+        std::cout << value << "\t";
+    }
+    std::cout << std::endl;
+
+
+    return 0;
+}
---
+++ advanced programing/L6 STL_2/Ефыл 2/Ефыл 2.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+#include <forward_list>
+#include <list>
+#include <map>
+#include <set>
+
+template<class T>
+void print_cont(const T& container) {
+
+    auto it = container.begin();
+    while (it != container.end()) {
+        std::cout << *it << "\t";
+        ++it;
+    }
+}
+
+template<class T, class U>
+void print_cont(const std::map<T, U> container) {
+
+    for (const auto& value : container) {
+        std::cout << value.first << ": " << value.second << "\t";
+    }
+}
+
+int main()
+{
+    std::vector<int> vec = { 1, 2, 3, 4 };
+    std::forward_list<int> forw_list = {10, 20, 30, 40};
+    std::list<int> lst = { 10, 20, 30, 40 };
+    std::set<char> st= { 'a', 'b', 'c', 'd'};
+    std::map<char, int> mp;
+
+    mp['a'] = 1;
+    mp['b'] = 3;
+    mp['r'] = 7;
+    mp['z'] = 5;
+
+    std::cout << "vector : ";
+    print_cont<std::vector<int>>(vec);
+
+    std::cout << "\nforward list : ";
+    print_cont<std::forward_list<int>>(forw_list);
+
+    std::cout << "\nlist : ";
+    print_cont<std::list<int>>(lst);
+
+    std::cout << "\nset : ";
+    print_cont<std::set<char>>(st);
+
+    std::cout << "\nmap : ";
+    print_cont<char, int>(mp);
+
+    return 0;
+}
