388baea7f64c5c500e63f2a55ceb8eb92f02762e
Nikita Sokolov <nekitsok10@yandex.ru>
2024-02-16 23:00:45 +0300

    complete l7 smart_ptr

---
+++ advanced programing/L7 smart_ptr/Task 3/Task 3.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+template <class T>
+class unique_ptr {
+private:
+    T* ptr;
+
+public:
+    unique_ptr(T* simple_ptr) : ptr(simple_ptr) {
+        ptr = new T;
+    }
+
+    unique_ptr(const unique_ptr<T>& un_ptr) = delete;
+    unique_ptr<T>& operator=(const unique_ptr<T>& un_ptr) = delete;
+
+    T& operator*() {
+        return *ptr;
+    }
+
+    T* release() {
+        T* rel_ptr = new T;
+        *rel_ptr = *ptr;
+
+        delete ptr;
+
+        return rel_ptr;
+    }
+
+    ~unique_ptr() {
+        delete ptr;
+    }
+};
+
+int main()
+{
+    unique_ptr<int> ptr_1(new int);
+    *ptr_1 = 12;
+    std::cout << *ptr_1 << std::endl;
+
+    //unique_ptr<int> ptr_2(ptr_1);
+    unique_ptr<int> ptr_2(new int);
+    *ptr_2 = 8;
+    //ptr_2 = ptr_1;
+
+    int* simple_ptr = ptr_2.release();
+    std::cout << *simple_ptr;
+
+    return 0;
+}
