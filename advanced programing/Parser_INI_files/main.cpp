02494bb97fa42608fce75a9efab0e359c02f49db
Nikita Sokolov <nekitsok10@yandex.ru>
2024-03-10 13:14:01 +0300

    complete L2_SQL

--- advanced programing/Parser_INI_files/main.cpp
+++ advanced programing/Parser_INI_files/main.cpp
@@ -13,8 +13,6 @@ private:
     std::map<std::string, int> int_vals;
     std::map<std::string, double> double_vals;
     std::map<std::string, std::string> string_vals;
-    std::map<std::string, int> names;
-
 public:
     void set_vals(std::map<std::string, std::string> init) {
 
@@ -33,15 +31,12 @@ public:
             }
 
             if (cifrs == val_pair.second.length()) {
-                names["int " + val_pair.first]++;
                 int_vals[val_pair.first] = std::stoi(val_pair.second);
             }
             else if (dot == 1 && cifrs == val_pair.second.length() - 1) {
-                names["double " + val_pair.first]++;
                 double_vals[val_pair.first] = std::stod(val_pair.second);
             }
             else {
-                names["string " + val_pair.first]++;
                 string_vals[val_pair.first] = val_pair.second;
             }
         }
@@ -59,23 +54,13 @@ public:
         for (const auto& pair : string_vals) {
             std::cout << pair.first << " = " << pair.second << std::endl;
         }
-    }
 
-    void print_names() {
-        for (const auto& pair : names) {
-            std::cout << pair.first << ", " << std::endl;
-        }
     }
 
     template <class T>
     T get_val(std::string name) {
 
-        std::string key = typeid(T).name();
-        key = key + " " + name; //иначе выдает ошибку о сумме 2 указателей
-       
-        if (!names.count(key)) {
-            throw std::invalid_argument("переменная " + name + " не найдена");
-        }
+        std::cout << this->int_vals.empty() << std::endl;
 
         T ansv;
 
@@ -90,19 +75,19 @@ public:
 class Parser {
 protected:
     std::map<std::string, std::unique_ptr<Section>> full;
+    std::map<std::string, std::map<std::string, std::string>> full_2;
     std::ifstream input_file;
     std::string kursor;
 
 public:
-    Parser(std::string filename) { input_file.open(filename); }
+    Parser(std::string filename) {
 
-    void read_file() {
+        input_file.open(filename);
+
         if (input_file.is_open()) {
 
-
             std::map<std::string, int> count_name_section;
-            std::string sec_name;
-            int number_line = 0;
+            std::string sec_name = "";
 
             bool flag = 1;
 
@@ -111,25 +96,14 @@ public:
 
                 if (flag) {
                     std::getline(input_file, kursor, '\n');
-                    number_line++;
                 }
 
                 if (kursor[0] == '[') {
                     int i = 1;
-                    sec_name = "";
-
-                    while (kursor[i] != ']' && i != kursor[i] - 1) {
+                    while (kursor[i] != ']') {
                         sec_name += kursor[i];
-                        if (kursor[i] == ';') {
-                            throw std::logic_error("Неверный синтаксис в строке " + std::to_string(number_line));
-                        }
                         i++;
                     }
-
-                    if (kursor[i] != ']') {
-                        throw std::logic_error("Неверный синтаксис в строке " + std::to_string(number_line));
-                    }
-
                     flag = 1;
                     continue;
                 }
@@ -138,41 +112,41 @@ public:
 
                     if (!flag) {
                         std::getline(input_file, kursor, '\n');
-                        number_line++;
                     }
-
+                    
 
                     if (kursor[0] != '[' && kursor[0] != ';' && kursor[0] != '\n') {
                         int pos = kursor.find("=");
-                        if (pos == kursor.length()) {
-                            throw std::logic_error("Неверный синтаксис в строке " + std::to_string(number_line));
-                        }
                         vals[kursor.substr(0, pos)] = kursor.substr(pos + 1, kursor.find(";") - pos - 1);
-                    }
+                    }  
 
                     flag = 0;
                     if (input_file.eof()) {
                         break;
                     }
                 }
-
+                
                 if (!vals.empty()) {
 
                     count_name_section[sec_name]++;
 
                     if (count_name_section[sec_name] == 1) {
 
-                        full[sec_name] = std::make_unique<Section>();
-                        full[sec_name]->set_vals(vals);
+                        full_2[sec_name] = vals;
+
+                        //full[sec_name] = std::make_unique<Section>();
+                        //full[sec_name]->set_vals(vals);
                     }
                     else {
-                        full[sec_name]->set_vals(vals);
+                        for (const auto& pair : vals) {
+                            full_2[sec_name][pair.first] = pair.second;
+                        }
+
+                        //full[sec_name]->set_vals(vals);
                     }
                 }
+                sec_name = "";
             }
-        }
-        else {
-            throw std::logic_error("file not found");
         }
     }
 
@@ -193,21 +167,46 @@ public:
         std::string name_sec = key.substr(0, key.find('.'));
         std::string name_val = key.substr(key.find('.') + 1, key.length());
 
-        T ansv;
+        T ansv = full[name_sec]->get_val<T>(name_val);
 
-        try {
-            ansv = full[name_sec]->get_val<T>(name_val);
-        }
-        catch (std::logic_error& err) {
-            std::cout << err.what() << std::endl;
-            std::cout << "Выберите переменную типа " << typeid(T).name() <<  " из доступных:" << std::endl;
-            full[name_sec]->print_names();
-            std::cin >> name_val;
-            ansv = full[name_sec]->get_val<T>(name_val);
-        }
-        
         return ansv;
 
+    }
+
+    template <class T>
+    T get_value_2(std::string key) {
+        std::string name_sec = key.substr(0, key.find('.'));
+        std::string name_val = key.substr(key.find('.') + 1, key.length());
+
+        std::string value = full_2[name_sec][name_val];
+
+        std::cout << value << std::endl;
+
+        T ansv;
+
+        int cifrs = 0;
+        int dot = 0;
+
+        for (const auto& symb : value) {
+            if (static_cast<int>(symb) >= static_cast<int>('0') && static_cast<int>(symb) <= static_cast<int>('9')) {
+                cifrs++;
+            }
+            if (static_cast<int>(symb) == static_cast<int>('.')) {
+                dot++;
+            }
+        }
+
+        if (cifrs == value.length()) {
+            if constexpr(std::is_same_v<T, int>) { ansv = std::stoi(value); }
+        }
+        else if (dot == 1 && cifrs == value.length() - 1) {
+            if constexpr(std::is_same_v<T, double>) { ansv = std::stod(value); }
+        }
+        else {
+            if constexpr(std::is_same_v<T, std::string>) { ansv = value; }
+        }
+
+        return ansv;
     }
 };
 
@@ -218,15 +217,14 @@ int main()
     SetConsoleCP(1251);
 
     Parser P1("input.ini");
-    try {
-        P1.read_file();
-    }
-    catch (std::logic_error& err) {
-        std::cout << err.what();
-        return 0;
-    }
 
-    int a = P1.get_value<int>("S1.var123");
+    //P1.print();
+
+    //P1.get_value<int>("s1.var1");
+
+    //int a = P1.get_value<int>("s1.var1");
+
+    int a = P1.get_value_2<int>("S1.var1");
 
     std::cout << a;
 
