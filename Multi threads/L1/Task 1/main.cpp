37326cdd148340fe731f33d0036d28089d26e565
Nikita Sokolov <nekitsok10@yandex.ru>
2024-04-18 16:59:23 +0300

    change L1 MAP

--- Multi threads/L1/Task 1/main.cpp
+++ Multi threads/L1/Task 1/main.cpp
@@ -1,52 +1,44 @@
 #include <iostream>
 #include <thread>
 
-using namespace std::chrono;
+bool flag = true;
+int max_counters = 8;
+int counter = 4;
 
-void imitClient(int& counter) {
-    counter++;
+void imitClient() {
+    while (flag) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        if (counter > max_counters) {
+            flag = 0;
+        }
+        counter++;
+    }
 }
 
-void imitWindow(int& counter) {
-    counter--;
+void imitWindow() {
+    while (counter > 0) {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        counter--;
+        std::cout << counter << std::endl;
+    }
 }
 
 int main() {
     auto start = std::chrono::steady_clock::now();
 
-    int counter = 4;
-    int max_counters = 8;
-    bool flag = true;
-    int i = 0;
-
     std::chrono::duration<double> dur;
 
-    while (counter > 0) {
+    std::thread t1(imitClient);
+    std::thread t2(imitWindow);
 
-        std::this_thread::sleep_for(1s);
-        i++;
-
-        if (flag) {
-            std::thread t1(imitClient, std::ref(counter));
-            t1.join();
-
-            if (counter > max_counters) {
-                flag = 0;
-            }
-        }
-
-        if (i % 2 == 0) {
-            std::thread t2(imitWindow, std::ref(counter));
-            t2.join();
-        }
-
-        std::cout << "counter = " << counter << std::endl;
-    }
+    t1.join();
+    t2.join();
 
     auto end = std::chrono::steady_clock::now();
     dur = end - start;
     std::cout << dur.count() << std::endl;
 
     std::cout << "counter = " << counter << std::endl;
+
     return 0;
 }
