d15d2720be48b886ecca018db257c0c71870cc68
Nikita Sokolov <nekitsok10@yandex.ru>
2024-05-10 10:14:42 +0300

    complete lesson 3

---
+++ Multi threads/L3/Task 1/Task 1.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <thread>
+#include <future>
+#include <chrono>
+#include <vector>
+
+void swap(int& a, int& b) {
+    int buf = a;
+    a = b;
+    b = buf;
+}
+
+void sort(std::vector<int> vec, std::promise<std::vector<int>> res) {
+    
+    for (int i = 0; i < vec.size(); i++) {
+        int min = vec[i];
+        int min_index = i;
+
+        for (int j = i + 1; j < vec.size(); j++) {
+            if (vec[j] < min) {
+                min = vec[j];
+                min_index = j;
+            }
+        }
+        swap(vec[i], vec[min_index]);
+    }
+
+    res.set_value(vec);
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    std::cout << "end swap" << std::endl;
+}
+
+
+int main()
+{
+    std::vector<int> vec;
+    vec.push_back(10);
+    vec.push_back(-20);
+    vec.push_back(1);
+    vec.push_back(5);
+    vec.push_back(3);
+
+    std::promise<std::vector<int>> pr;
+    auto pr_future(pr.get_future());
+    auto calc(std::async(sort, vec, std::move(pr)));
+
+    std::vector<int> ansv = pr_future.get();
+
+    for (const auto& val : ansv) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ Multi threads/L3/Task 2/Task 2.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+#include <chrono>
+#include <thread>
+
+int elems_in_block = 4;
+
+void calc_foo(std::vector<int>::iterator it_begin, std::vector<int>::iterator it_end) {
+    
+    while (it_begin != it_end) {
+        *it_begin = *it_begin * 2;
+        it_begin++;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
+
+void for_each(std::vector<int>::iterator it_begin, std::vector<int>::iterator it_end,
+        void(*foo)(std::vector<int>::iterator, std::vector<int>::iterator)) {
+
+    int i = 1;
+    std::vector<int>::iterator it_buf = it_begin;
+    while (i != elems_in_block && it_buf != it_end) {
+        i++;
+        it_buf++;
+    }
+
+    auto calc{std::async(foo, it_begin, it_buf)};
+
+    if (it_buf != it_end) {
+        for_each(it_buf, it_end, foo);
+        it_buf++;
+    }
+
+    calc.wait();
+}
+
+void for_each_one_thread(std::vector<int>::iterator it_begin, std::vector<int>::iterator it_end,
+    void(*foo)(std::vector<int>::iterator, std::vector<int>::iterator)) {
+    
+    int i = 1;
+    std::vector<int>::iterator it_buf = it_begin;
+    while (i != elems_in_block && it_buf != it_end) {
+        i++;
+        it_buf++;
+    }
+
+    foo(it_begin, it_buf);
+
+    if (it_buf != it_end) {
+        for_each_one_thread(it_buf, it_end, foo);
+        it_buf++;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::vector<int> vec;
+    for (int i = 0; i < 30; ++i) {
+        vec.push_back(i);
+    }
+
+    std::cout << "Контейнер до преобразования:" << std::endl;
+    for (const auto& val : vec) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+
+    auto start = std::chrono::steady_clock::now();
+
+    //for_each_one_thread(vec.begin(), vec.end(), calc_foo);
+    for_each(vec.begin(), vec.end(), calc_foo);
+
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> dur = end - start;
+
+    std::cout << "Контейнер после преобразования" << std::endl;
+    for (const auto& val : vec) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Время выполнения = " << dur.count() << std::endl;
+
+    return 0;
+}
