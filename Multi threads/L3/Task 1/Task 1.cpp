a694f2de54d5091fe1b8b7a8cee3111e8a3368ee
Nikita Sokolov <nekitsok10@yandex.ru>
2024-05-10 12:49:07 +0300

    correct L3 t1

--- Multi threads/L3/Task 1/Task 1.cpp
+++ Multi threads/L3/Task 1/Task 1.cpp
@@ -10,19 +10,25 @@ void swap(int& a, int& b) {
     b = buf;
 }
 
+int find_min(const std::vector<int>& vec, int start_pos) {
+    int min = vec[start_pos];
+    int min_index = start_pos;
+
+    for (int j = start_pos + 1; j < vec.size(); j++) {
+        if (vec[j] < min) {
+            min = vec[j];
+            min_index = j;
+        }
+    }
+
+    return min_index;
+}
+
 void sort(std::vector<int> vec, std::promise<std::vector<int>> res) {
     
     for (int i = 0; i < vec.size(); i++) {
-        int min = vec[i];
-        int min_index = i;
-
-        for (int j = i + 1; j < vec.size(); j++) {
-            if (vec[j] < min) {
-                min = vec[j];
-                min_index = j;
-            }
-        }
-        swap(vec[i], vec[min_index]);
+        auto min_index{ std::async(find_min, std::ref(vec), i) };
+        swap(vec[i], vec[min_index.get()]);
     }
 
     res.set_value(vec);
@@ -34,6 +40,8 @@ void sort(std::vector<int> vec, std::promise<std::vector<int>> res) {
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+
     std::vector<int> vec;
     vec.push_back(10);
     vec.push_back(-20);
@@ -41,12 +49,20 @@ int main()
     vec.push_back(5);
     vec.push_back(3);
 
+    std::cout << "До сортировки:" << std::endl;
+    for (const auto& val : vec) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+
+
     std::promise<std::vector<int>> pr;
     auto pr_future(pr.get_future());
     auto calc(std::async(sort, vec, std::move(pr)));
 
     std::vector<int> ansv = pr_future.get();
 
+    std::cout << "После сортировки:" << std::endl;
     for (const auto& val : ansv) {
         std::cout << val << " ";
     }
